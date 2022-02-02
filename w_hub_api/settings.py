46fd21cb89b72a693d93231fdcc240ae384be49e
FayiZ33 <muhammedfayiz002@gmail.com>
2022-02-02 21:49:08 +0530

    env

--- w_hub_api/settings.py
+++ w_hub_api/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-@m4+@zunr)5r98a+l@$%sxaab9p$96d2ur!ch^le9j%=-c%lx^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS'),'127.0.0.1']
 
 
 # Application definition
@@ -103,19 +105,14 @@ WSGI_APPLICATION = 'w_hub_api.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    # 'emp_db':{
-    #     'ENGINE':'django.db.backends.postgresql',
-    #     'NAME':'db1',
-    #     'USER':'fz',
-    #     'PASSWORD':'hesoyam',
-    #     'HOST':'localhost',
-    #     'PORT':5432
-    # },
+        'ENGINE': config('ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT')
+    }
 }
 
 
